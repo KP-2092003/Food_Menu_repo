531e50505c0375816e1ab461482fbc7228072a3c
kunal_2003_pal <kunalpal2092003@gmail.com>
2025-03-11 22:02:14 +0530

    Initial commit

---
+++ food/urls.py
@@ -0,0 +1,16 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    #/food/
+    path('', views.IndexClassView.as_view, name='index'),
+    #/food/1
+    path('<int:pk>/', views.FoodDetail.as_view, name='detail'),
+    path('item/', views.item, name='item'),
+    # add items
+    path('add', views.CreateItem.as_view, name='create_item'),
+    # edit item
+    path('update/<int:id>/', views.update_item, name='update_item'),
+    # Delete item
+    path('delete/<int:id>/', views.delete_item, name='delete_item'),
+]
